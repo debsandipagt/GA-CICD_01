c23b9f8cd987eb504a7b010f3c59f0cfd17fa9da
Sandip Deb <debsandip.agt@gmail.com>
2025-01-16 16:53:46 +0530

    second commit

--- test_app.py
+++ test_app.py
@@ -5,6 +5,7 @@ from app import APP
 
 class Tests(unittest.TestCase):
     '''Basic tests for the application'''
+
     def setUp(self):
         '''Create a test client for the app'''
         self.app = APP.test_client()
@@ -12,12 +13,12 @@ class Tests(unittest.TestCase):
     def test_200(self):
         '''test_200: a request for / shall return 200 OK'''
         res = self.app.get('/')
-        assert res.status == '200 OK'
+        self.assertEqual(res.status, '200 OK', "Expected status code 200 OK")
 
     def test_404(self):
         '''test_404: a request for null shall return 404 NOT FOUND'''
         res = self.app.get('/null')
-        assert res.status == '404 NOT FOUND'
+        self.assertEqual(res.status, '404 NOT FOUND', "Expected status code 404 NOT FOUND")
 
 
 if __name__ == "__main__":
