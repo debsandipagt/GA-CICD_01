4cc00043af7af20bdbbb425eb032daf25eaa91f6
Sandip Deb <debsandip.agt@gmail.com>
2025-01-16 17:16:59 +0530

    fifth commit

--- app.py
+++ app.py
@@ -1,25 +1,49 @@
-'''A module for testing'''
-import unittest
-from app import APP
+#!/usr/bin/env python
+"""
+mascot: a microservice for serving mascot data
+"""
+import json
+from flask import Flask, jsonify, abort, make_response
+
+APP = Flask(__name__)
+
+# Load the data
+MASCOTS = json.load(open('data.json', 'r'))
 
 
-class Tests(unittest.TestCase):
-    '''Basic tests for the application'''
-
-    def setUp(self):
-        '''Create a test client for the app'''
-        self.app = APP.test_client()
-
-    def test_200(self):
-        '''test_200: a request for / shall return 200 OK'''
-        with self.app.get('/') as res:
-            self.assertEqual(res.status, '200 OK', "Expected status code 200 OK")
-
-    def test_404(self):
-        '''test_404: a request for null shall return 404 NOT FOUND'''
-        with self.app.get('/null') as res:
-            self.assertEqual(res.status, '404 NOT FOUND', "Expected status code 404 NOT FOUND")
+@APP.route('/', methods=['GET'])
+def get_mascots():
+    """
+    Function: get_mascots
+    Input: none
+    Returns: A list of mascot objects
+    """
+    return jsonify(MASCOTS)
 
 
-if __name__ == "__main__":
-    unittest.main()
+@APP.route('/<guid>', methods=['GET'])
+def get_mascot(guid):
+    """
+    Function: get_mascot
+    Input: a mascot GUID
+    Returns: The mascot object with GUID matching the input
+    """
+    for mascot in MASCOTS:
+        if guid == mascot['guid']:
+            return jsonify(mascot)
+    abort(404)
+    return None
+
+
+@APP.errorhandler(404)
+def not_found(error):
+    """
+    Function: not_found
+    Input: The error
+    Returns: HTTP 404 with r
+    """
+    return make_response(jsonify({'error': str(error)}), 404)
+
+
+if __name__ == '__main__':
+    APP.run("0.0.0.0", port=8080, debug=True)
--- test_app.py
+++ test_app.py
@@ -12,13 +12,13 @@ class Tests(unittest.TestCase):
 
     def test_200(self):
         '''test_200: a request for / shall return 200 OK'''
-        res = self.app.get('/')
-        self.assertEqual(res.status, '200 OK', "Expected status code 200 OK")
+        with self.app.get('/') as res:
+            self.assertEqual(res.status, '200 OK', "Expected status code 200 OK")
 
     def test_404(self):
         '''test_404: a request for null shall return 404 NOT FOUND'''
-        res = self.app.get('/null')
-        self.assertEqual(res.status, '404 NOT FOUND', "Expected status code 404 NOT FOUND")
+        with self.app.get('/null') as res:
+            self.assertEqual(res.status, '404 NOT FOUND', "Expected status code 404 NOT FOUND")
 
 
 if __name__ == "__main__":
